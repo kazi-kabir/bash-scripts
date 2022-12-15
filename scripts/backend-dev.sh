c0626369e64c6e9951d28a792db35cdadc707e68
kazi-kabir <kazi.s221999@gmail.com>
2022-12-15 21:35:42 +1100

    backend script done

--- scripts/backend-dev.sh
+++ scripts/backend-dev.sh
@@ -1,3 +1,7 @@
 #TODO
 
 #Open intellij, postman, and chrome
+
+open -na "IntelliJ IDEA CE" --args --new-window & 
+open -na "Google Chrome" --args --new-window & 
+open -na "Postman" --args --new-window
