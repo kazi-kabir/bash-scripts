120b5f628abd4808d104b39aee5583b40742334f
kazi-kabir <kazi.s221999@gmail.com>
2022-12-15 21:42:22 +1100

    remove comments

--- stop.sh
+++ stop.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
 
-# Get the list of all running processes
 processes=$(ps aux)
 
-# Loop through each process and kill it
 while read -r line; do
-  # Get the process ID
   pid=$(echo $line | awk '{print $2}')
 
-  # Send the SIGKILL signal to the process
   kill -9 $pid
 done <<< "$processes"
