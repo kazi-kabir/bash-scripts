fe60460ab187085bc92a59f9cfc2a7f7f54eb3f4
kazi-kabir <kazi.s221999@gmail.com>
2022-12-17 11:27:54 +1100

    removed apple script for now

--- scripts/web-dev-env.sh
+++ scripts/web-dev-env.sh
@@ -3,13 +3,3 @@
 open -na "Visual Studio Code" --args --new-window & 
 open -na "Google Chrome" --args --new-window
 
-osascript <<EOF
-tell application "Google Chrome"
-    set windowList to every window
-    repeat with theWindow in windowList
-        set theID to id of theWindow
-        log theID
-    end repeat
-end tell
-EOF
-
