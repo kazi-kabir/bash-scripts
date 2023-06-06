b8de55ee62ab630665718192f72b951b2f991047
kazi-kabir <kazi.s221999@gmail.com>
2023-06-07 01:58:20 +0600

    does it work?

---
+++ mission-control.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Open terminal with "Kazi (prod)" profile
+gnome-terminal --window --profile="Kazi (prod)"
+
+# Open terminal with "Kazi (~)" profile
+gnome-terminal --window --profile="Kazi (~)"
