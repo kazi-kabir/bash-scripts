ed927b4471915192a84dd0164a01e1bb0006ba27
kazi-kabir <kazi.s221999@gmail.com>
2023-06-07 19:59:18 +0600

    update comments

--- mission-control.sh
+++ mission-control.sh
@@ -2,8 +2,8 @@
 
 # you have to define the paths
 
-# Open Chrome with "Kazi (prod)" profile
+# Open Chrome with specified profile 
 open -a "Google Chrome" --args --profile-directory="Default"
 
-# Open Chrome with "Kazi (~)" profile
+# Open Chrome with specified profile
 open -a "Google Chrome" --args --profile-directory="Profile 1"
